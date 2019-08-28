9b6ea56c831519cbcae9b0c1273c1e286b065e76
phuoc <phuocpro1969@google.com>
2019-08-29 03:40:21 +0700

    update

--- bookstore/src/main/java/pq/jdev/b001/bookstore/users/web/PasswordFogotController.java
+++ bookstore/src/main/java/pq/jdev/b001/bookstore/users/web/PasswordFogotController.java
@@ -1,6 +1,7 @@
 package pq.jdev.b001.bookstore.users.web;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -9,7 +10,9 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +24,7 @@ import pq.jdev.b001.bookstore.users.model.Mail;
 import pq.jdev.b001.bookstore.users.model.PasswordResetToken;
 import pq.jdev.b001.bookstore.users.model.Person;
 import pq.jdev.b001.bookstore.users.service.EmailService;
+import pq.jdev.b001.bookstore.users.service.ModuleRunFirst;
 import pq.jdev.b001.bookstore.users.service.UserService;
 import pq.jdev.b001.bookstore.users.web.dto.PasswordForgotDto;
 
@@ -30,9 +34,12 @@ import pq.jdev.b001.bookstore.users.web.dto.PasswordForgotDto;
 public class PasswordFogotController {
 	@Autowired
 	private UserService userService;
-	
+
 	@Autowired
 	private EmailService emailService;
+
+	@Autowired
+	private ModuleRunFirst moduleRunFirst;
 
 	@ModelAttribute("forgotPasswordForm")
 	public PasswordForgotDto forgotPasswordDto() {
@@ -40,50 +47,52 @@ public class PasswordFogotController {
 	}
 
 	@GetMapping
-	public String displayForgotPasswordPage(ModelMap map) {
-		map.addAttribute("header", "header_login");
-		map.addAttribute("footer", "footer_login");
+	public String displayForgotPasswordPage(ModelMap map, Model model, Authentication authentication) {
+		List<String> roles = moduleRunFirst.getRole(authentication);
+		moduleRunFirst.leftBar_cate_pub(model);
+		moduleRunFirst.headerFooter(authentication, map, roles);
 		return "user_admin/no_login/forgot-password";
 	}
 
 	@PostMapping
 	public String processForgotPasswordForm(@ModelAttribute("forgotPasswordForm") @Valid PasswordForgotDto form,
-			BindingResult result, HttpServletRequest request, ModelMap map) {
+			BindingResult result, HttpServletRequest request, ModelMap map, Model model,
+			Authentication authentication) {
+		List<String> roles = moduleRunFirst.getRole(authentication);
+		moduleRunFirst.leftBar_cate_pub(model);
+		moduleRunFirst.headerFooter(authentication, map, roles);
 
 		if (result.hasErrors()) {
-			map.addAttribute("header", "header_login");
-			map.addAttribute("footer", "footer_login");
+			moduleRunFirst.leftBar_cate_pub(model);
+			moduleRunFirst.headerFooter(authentication, map, roles);
 			return "user_admin/no_login/forgot-password";
 		}
-		
-		map.addAttribute("header", "header_login");
-		map.addAttribute("footer", "footer_login");
-		
+
 		Person person = userService.findByEmail(form.getEmail());
 		if (person == null) {
 			result.rejectValue("email", null, "We could not find an account for that e-mail address.");
 			return "user_admin/no_login/forgot-password";
 		}
-		
+
 		PasswordResetToken token = new PasswordResetToken();
 		token.setToken(UUID.randomUUID().toString());
 		token.setPerson(person);
 
 		token.setExpiryDate();
 		userService.saveToken(token);
-		
+
 		Mail mail = new Mail();
 		mail.setFrom("user1@testmail.com");
 		mail.setTo(person.getEmail());
 		mail.setSubject("Password reset request");
 
-		Map<String, Object> model = new HashMap<>();
-		model.put("token", token);
-		model.put("person", person);
-		model.put("signature", "http://localhost:8090");
+		Map<String, Object> modelMap = new HashMap<>();
+		modelMap.put("token", token);
+		modelMap.put("person", person);
+		modelMap.put("signature", "http://localhost:8090");
 		String url = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort();
-		model.put("resetUrl", url + "/reset-password?token=" + token.getToken());
-		mail.setModel(model);
+		modelMap.put("resetUrl", url + "/reset-password?token=" + token.getToken());
+		mail.setModel(modelMap);
 		emailService.sendEmail(mail);
 
 		return "redirect:/forgot-password?success";
